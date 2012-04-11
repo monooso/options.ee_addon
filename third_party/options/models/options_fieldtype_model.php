da4c5a5107053f1b6940734e9150a4c1fa53fcb2
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-04-11 23:04:15 +0100

    Create and destroy the fieldtype database table.

--- third_party/options/models/options_fieldtype_model.php
+++ third_party/options/models/options_fieldtype_model.php
@@ -35,6 +35,65 @@ class Options_fieldtype_model extends Options_model {
   }
 
 
+  /**
+   * Creates the fieldtype database table(s).
+   *
+   * @access  public
+   * @return  void
+   */
+  public function create_fieldtype_tables()
+  {
+    $this->EE->load->dbforge();
+
+    $fields = array(
+      'data_source_id' => array(
+        'auto_increment'  => TRUE,
+        'constraint'      => 10,
+        'type'            => 'INT',
+        'unsigned'        => TRUE
+      ),
+      'site_id' => array(
+        'constraint'  => 5,
+        'type'        => 'INT',
+        'unsigned'    => TRUE
+      ),
+      'data_source_format' => array(
+        'constraint'  => 20,
+        'type'        => 'VARCHAR'
+      ),
+      'data_source_location' => array(
+        'constraint'  => 255,
+        'type'        => 'VARCHAR'
+      ),
+      'data_source_title' => array(
+        'constraint'  => 255,
+        'type'        => 'VARCHAR'
+      ),
+      'data_source_type' => array(
+        'constraint'  => 10,
+        'type'        => 'VARCHAR'
+      )
+    );
+
+    $this->EE->dbforge->add_field($fields);
+    $this->EE->dbforge->add_key('data_source_id', TRUE);
+    $this->EE->dbforge->create_table('options_data_sources', TRUE);
+  }
+
+
+  /**
+   * Destroys the fieldtype database table(s).
+   *
+   * @access  public
+   * @return  void
+   */
+  public function destroy_fieldtype_tables()
+  {
+    $this->EE->load->dbforge();
+    $this->EE->dbforge->drop_table('options_data_sources');
+  }
+
+
 }
 
 
